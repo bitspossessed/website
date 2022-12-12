7e535d34d7ee611160fdd042d2c0d7eeffab76e1
Mikolaj Zimny <mikolaj.zimny@bridgemaker.com>
2022-12-12 14:56:19 +0400

    Add mixing color functions

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,5 @@
 function randomRange(min, max) {
-  return Math.round(min + (Math.random() * (max - min)));
+  return Math.round(min + Math.random() * (max - min));
 }
 
 function generateRandomPath(width, height, randomness) {
@@ -56,7 +56,7 @@ function generateRandomPath(width, height, randomness) {
 
     if (index === 0) {
       d.push(`M${coord.x},${coord.y}`);
-      p.push(array[array.length - 3])
+      p.push(array[array.length - 3]);
       p.push(array[index]);
       p.push(array[index + 1]);
       p.push(array[index + 2]);
@@ -128,9 +128,127 @@ function getShapeElement(options) {
 }
 
 const shapes = document.getElementById('shapes');
-shapes.appendChild(getShapeElement({
-  width: 500,
-  height: 500,
-  randomness: 50,
-  colors: ['#ed6ea0', '#ec8c69'],
-}));
+
+const colorElectricViolet = '#B100FF'; //rgb(177, 0, 255)
+const colorPersianBlue = '#1116C8'; //rgb(17, 22, 200)
+const colorBlackRussian = '#020024'; //rgb(2, 0, 36)
+const colorDeepBlush = '#ED6EA0'; //rgb(237, 110, 160)
+
+function hslToHex(h, s, l) {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, '0'); // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+function hexToHSL(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  r = parseInt(result[1], 16);
+  g = parseInt(result[2], 16);
+  b = parseInt(result[3], 16);
+  (r /= 255), (g /= 255), (b /= 255);
+  var max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  var h,
+    s,
+    l = (max + min) / 2;
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+  var HSL = new Object();
+  HSL['h'] = h;
+  HSL['s'] = s;
+  HSL['l'] = l;
+  return HSL;
+}
+
+function generateShadesColors(hexColor) {
+  const hexColorHSL = hexToHSL(hexColor);
+
+  const firstShade = 'hsl(' + parseFloat(hexColorHSL.h.toFixed(2)) * 100 + ', ' + parseFloat(hexColorHSL.s.toFixed(2)) * 100 + '%, ' + 90 + '%)';
+  const secondShade = 'hsl(' + parseFloat(hexColorHSL.h.toFixed(2)) * 100 + ', ' + parseFloat(hexColorHSL.s.toFixed(2)) * 100 + '%, ' + 70 + '%)';
+  const thirdShade = 'hsl(' + parseFloat(hexColorHSL.h.toFixed(2)) * 100 + ', ' + parseFloat(hexColorHSL.s.toFixed(2)) * 100 + '%, ' + 50 + '%)';
+  const fourthShade = 'hsl(' + parseFloat(hexColorHSL.h.toFixed(2)) * 100 + ', ' + parseFloat(hexColorHSL.s.toFixed(2)) * 100 + '%, ' + 30 + '%)';
+  const fifthShade = 'hsl(' + parseFloat(hexColorHSL.h.toFixed(2)) * 100 + ', ' + parseFloat(hexColorHSL.s.toFixed(2)) * 100 + '%, ' + 10 + '%)';
+
+  const shades = [firstShade, secondShade, thirdShade, fourthShade, fifthShade];
+
+  console.log('shades', shades);
+  return shades;
+}
+
+function generateAnaloguesColors(hexColor) {
+  const hexColorHSL = hexToHSL(hexColor);
+  const h = parseFloat(hexColorHSL.h.toFixed(2)) * 100;
+  const s = parseFloat(hexColorHSL.s.toFixed(2)) * 100;
+  const l = parseFloat(hexColorHSL.l.toFixed(2)) * 100;
+
+  function correctiveHue(x) {
+    if (h + x > 360) {
+      return h + x - 360;
+    } else {
+      return h + x;
+    }
+  }
+
+  function correctiveSat(x) {
+    if (s + x > 100) {
+      return s + x - 100;
+    } else {
+      return s + x;
+    }
+  }
+
+  function correctiveLight(x) {
+    if (l + x > 100) {
+      return l - x;
+    } else {
+      return l + x;
+    }
+  }
+
+  const firstAnalogues = 'hsl(' + correctiveHue(-60) + ', ' + correctiveSat(-15) + '%, ' + correctiveLight(10) + '%)';
+  const secondAnalogues = 'hsl(' + correctiveHue(-30) + ', ' + correctiveSat(15) + '%, ' + correctiveLight(10) + '%)';
+  const thirdAnalogues = 'hsl(' + h + ', ' + s + '%, ' + l + '%)';
+  const fourthAnalogues = 'hsl(' + correctiveHue(30) + ', ' + correctiveSat(-15) + '%, ' + correctiveLight(10) + '%)';
+  const fifthAnalogues = 'hsl(' + correctiveHue(60) + ', ' + correctiveSat(15) + '%, ' + correctiveLight(10) + '%)';
+
+  const analogues = [firstAnalogues, secondAnalogues, thirdAnalogues, fourthAnalogues, fifthAnalogues];
+
+  console.log('analogues', analogues);
+
+  return analogues;
+}
+
+generateShadesColors('#ed6ea0');
+generateAnaloguesColors('#ed6ea0');
+
+shapes.appendChild(
+  getShapeElement({
+    width: 500,
+    height: 500,
+    randomness: 50,
+    colors: generateAnaloguesColors('#ed6ea0'),
+  }),
+);
